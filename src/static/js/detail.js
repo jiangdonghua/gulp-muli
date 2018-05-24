1fa425119986edccc41044dc57a58b2127f3d5c9
jiangdonghua <861252878@qq.com>
2018-05-25 00:59:08 +0800

    多页面的gulp配置

---
+++ src/static/js/detail.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2018/5/24.
+ */
+console.log('this');
+console.log('a');
+console.log('details');
+console.log('page');
+console.log('ha');
---
+++ src/static/js/index.js
@@ -0,0 +1,9 @@
+var swiper1 = new Swiper('.swiper-container-banner');
+var swiper = new Swiper('.swiper-picScroll', {
+    slidesPerView: 3,
+    spaceBetween:15,
+//        autoplay:true
+});
+console.log('i love 520')
+console.log('i love 521')
+

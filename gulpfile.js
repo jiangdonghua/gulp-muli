9dd6cc447dc6d9553a885219b0b783b6573028da
jiangdonghua <861252878@qq.com>
2018-05-25 01:36:30 +0800

    .

--- gulpfile.js
+++ gulpfile.js
@@ -233,10 +233,10 @@ gulp.task('default', () => {
     生产环境： npm run build
     执行压缩： gulp zip
     编译页面： gulp html
-    编译脚本： gulp script
+    编译脚本： gulp scripts
     编译样式： gulp styles
-    语法检测： gulp eslint
     压缩图片： gulp images
+    ...其他看注释
     `
     ))
 })
